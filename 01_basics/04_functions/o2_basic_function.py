66e3fe0b5c777e5193926e64cf1bf70421bb0d7f
aviikc <avik.c@whistlingwoods.net>
2020-04-11 08:22:02 +0530

    update

---
+++ 01_basics/04_functions/o2_basic_function.py
@@ -0,0 +1,13 @@
+def my_function():
+    print("Hello")
+
+def my_new_function(name):
+    print(f'Hello {name}')
+
+def max_number(my_list):
+    print(f'The maximum value is: {max(my_list)}')
+
+
+if __name__ == "__main__":
+    list_a = [3,56,23,45,6474]
+    max_number(list_a)
